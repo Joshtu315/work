eb41f6abd130a299eb295c240315080d2b752e58
Joshtu315 <36805636+Joshtu315@users.noreply.github.com>
2018-04-19 18:37:07 -0400

    Update Exerciese.py

--- Exerciese.py
+++ Exerciese.py
@@ -1,3 +1,20 @@
+class Wallet():
+    def __init__(self, x):
+        self.money=x
+    def add(self, x):
+        self.money+=x
+    def view(self):
+        print(self.money)
+
+w=Wallet(100)
+w.add(5)
+w.view()
+
+w.add(3)
+w.view()
+
+
+
 ##########################################################################
 # 1. two sum
 # solution 1
@@ -138,7 +155,7 @@ class Solution(object):
 
 
 ##########################################################################
-#26. Remove Duplicates from Sorted Array
+# 26. Remove Duplicates from Sorted Array
 class Solution(object):
     def removeDuplicates(self,A):
         if not A:
@@ -152,7 +169,7 @@ class Solution(object):
     
 
 ##########################################################################
-#27. Remove Element
+# 27. Remove Element
 class Solution:
     def removeElement(self, nums, val): 
         while val in nums:
@@ -160,7 +177,7 @@ class Solution:
 
 
 ##########################################################################
-#28. Implement strStr()
+# 28. Implement strStr()
 class Solution:
     def strStr(self, haystack, needle):
         try:
@@ -170,7 +187,7 @@ class Solution:
 
 
 ##########################################################################
-#35. Search Insert Position
+# 35. Search Insert Position
 class Solution:
     def searchInsert(self, nums, target):
         for i in nums:
@@ -181,10 +198,173 @@ class Solution:
 
 
 ##########################################################################
-#53. Maximum Subarray
+# 53. Maximum Subarray
 class Solution:
-    def maxSubArray(self, nums):
-        for i in nums:
-            if i < 0:
-                nums.remove(i)
-        return sum(nums)
+    def maxSubArray(self, A):
+        if not A:
+            return 0
+        curSum = maxSum = A[0]
+        for num in A[1:]:
+            curSum = max(num, curSum + num)
+            maxSum = max(maxSum, curSum)
+
+        return maxSum
+
+
+
+##########################################################################
+# 58. Length of Last Word
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        retrun len(s.strip().split(" ")[-1])
+
+
+##########################################################################
+# 66. Plus One
+class Solution(object):
+    def plusOne(self, digits):
+        n=0
+        for i, ch in enumerate(digits):
+            n+=(ch*(10**(len(digits)-1-i)))
+        return([int(i) for i in str(n+1)])
+
+
+
+##########################################################################
+# 69. Sqrt(x)
+class Solution(object):
+    def mySqrt(self, x):
+        r = x
+        while r*r > x:
+            r = (r + x/r) / 2
+        return int(r)
+
+
+##########################################################################
+# 70. Climbing Stairs
+class Solution(object):
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        res = [0 for i in xrange(n)]
+        res[0], res[1] = 1, 2
+        for i in xrange(2, n):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]
+        
+
+##########################################################################
+# 83. Remove Duplicates from Sorted List
+class Solution(object):
+    def deleteDuplicates(self, head):
+            if head is None:
+                return head
+            curr = head
+            while curr.next:
+                if curr.next.val == curr.val:
+                    curr.next = curr.next.next
+                else:
+                    curr = curr.next
+            return head
+
+
+##########################################################################
+# 88. Merge Sorted Array
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[m:]=nums2[:n]
+        return nums1.sort()
+
+
+##########################################################################
+#100. Same Tree
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p is q
+
+
+##########################################################################
+# 118. Pascal's Triangle
+class Solution:
+    def generate(self, numRows):
+        lists = []
+        for i in range(numRows):
+            lists.append([1]*(i+1))
+            if i>1 :
+                for j in range(1,i):
+                    lists[i][j]=lists[i-1][j-1]+lists[i-1][j]
+        return lists
+
+
+
+##########################################################################
+# 119. Pascal's Triangle II
+class Solution:
+    def getRow(self, rowIndex):
+        lists = []
+        for i in range(rowIndex+1):
+            lists.append([1]*(i+1))
+            if i>1 :
+                for j in range(1,i):
+                    lists[i][j]=lists[i-1][j-1]+lists[i-1][j]
+        return lists[-1]
+
+
+
+##########################################################################
+# 121. Best Time to Buy and Sell Stock
+class Solution(object):
+    def maxProfit(self,prices):
+        max_profit, min_price = 0, float('inf')
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        return max_profit
+
+
+##########################################################################
+# 122. Best Time to Buy and Sell Stock II
+
+class Solution(object):
+    def maxProfit(self, prices):
+        maxpro=0
+        for i in range(0,len(prices)-1):
+            maxpro+=max(prices[i+1] -prices[i],0)
+        return maxpro
+
+
+
+
+
+##########################################################################
+# 125. Valid Palindrome
+class Solution(object):
+    def isPalindrome(self, s):
+        if not s:
+            return True
+        i = []
+        for each in s:
+            if each.isalnum():
+                i.extend(each.lower())
+        return i == i[::-1]
+
+
+
+##########################################################################
+# 136. Single Number
+class Solution(object):
+    def singleNumber(self, nums):
+        return sum(set(nums))*2 - sum(nums)
+
+
+
