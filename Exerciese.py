63482e5b7dbf584c7552969a0456f0574de76d1d
Joshtu315 <36805636+Joshtu315@users.noreply.github.com>
2018-06-11 16:27:00 -0400

    Update Exerciese.py

--- Exerciese.py
+++ Exerciese.py
@@ -632,6 +632,16 @@ ON P1.Id = P2.Id
 WHERE P2.Id Is Null
 
 
+##########################################################################
+# 262. Trips and Users
+select Request_at as Day, 
+round(sum(case when Status like 'cancelled_%' then 1 else 0 end) / count(*),2) as 'Cancellation Rate'
+from Trips a 
+join Users b on a.Client_Id = b.Users_Id 
+where Request_at between '2013-10-01' and '2013-10-03' and Banned ='No'
+group by Request_at
+
+
 
 ##########################################################################
 # 601. Human Traffic of Stadium
